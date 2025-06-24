116f30e9dc37c8173c3dee823976c704e1cc4342
mensahJr <christianamoakomensahyeboah@gmail.com>
2025-06-24 17:54:19 +0000

    fifth commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("that's cool")
+    println("alright")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
